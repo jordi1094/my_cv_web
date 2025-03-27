38d7a22c5211a2817fa3bb76d6bd45be16f8ced3
jordi1094 <63929965+jordi1094@users.noreply.github.com>
2025-03-27 21:15:39 +0100

    image more stetic

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,7 +15,7 @@ const Home: React.FC = () => {
         <Header/>
         <main className="grid grid-cols-3 gap-2 mx-24 pb-7">
             <AboutMe/>
-            <img src="/images/profile-image.jpg" className="rounded-lg h-[65vh]"></img>
+            <img src="/images/profile-image.jpg" className="rounded-lg h-[65vh] w-[fill] object-cover"></img>
             <MasterTools/>
             <IrrigationAlert/>
             <HomeClimateBot/>
