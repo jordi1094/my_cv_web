17b529bf6c297096644c655ff74c14488097a3f9
jordi1094 <63929965+jordi1094@users.noreply.github.com>
2025-03-27 21:11:55 +0100

    ready to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, {useState, useEffect} from 'react'
-import { Route, Routes,Navigate } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 
