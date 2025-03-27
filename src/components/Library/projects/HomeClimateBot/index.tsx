8169e9c4fcb114bf7694dbc95ab15f844edbe9c1
jordi1094 <63929965+jordi1094@users.noreply.github.com>
2025-03-27 21:10:29 +0100

    delete files to start be able to deploy

--- src/components/Library/projects/HomeClimateBot/index.tsx
+++ src/components/Library/projects/HomeClimateBot/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useTranslation, UseTranslation } from "react-i18next";
+import { useTranslation} from "react-i18next";
 import '../../../../i18n'
 
 const HomeClimateBot: React.FC = () => {
--- src/components/core/Field_input/field_input.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Label from "../Label/label";
-
-interface FieldInputProps{
-
-}
-
-const FieldInput:_React.FC<FieldInputsProps> = ({id, type, placeholder,className})
